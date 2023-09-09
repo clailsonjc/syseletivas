8981a150128b0d1980cc46f2a12056df44823e8f
Clailson <clailsonjc@gmail.com>
2023-09-09 13:37:33 -0300

    criação dominio aluno

---
+++ src/main/java/com/sys/eletivas/dmain/Aluno.java
@@ -0,0 +1,79 @@
+package com.sys.eletivas.dmain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Aluno implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long idAluno;
+	private Long matrícula;
+	private String nome;
+	private Integer sexo;
+	private String senha;
+	
+	public Aluno(Long idAluno, Long matrícula, String nome, Integer sexo, String senha) {
+		super();
+		this.idAluno = idAluno;
+		this.matrícula = matrícula;
+		this.nome = nome;
+		this.sexo = sexo;
+		this.senha = senha;
+	}
+	
+		
+	public Long getIdAluno() {
+		return idAluno;
+	}
+	public void setIdAluno(Long idAluno) {
+		this.idAluno = idAluno;
+	}
+	public Long getMatrícula() {
+		return matrícula;
+	}
+	public void setMatrícula(Long matrícula) {
+		this.matrícula = matrícula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getSexo() {
+		return sexo;
+	}
+	public void setSexo(Integer sexo) {
+		this.sexo = sexo;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idAluno);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		return Objects.equals(idAluno, other.idAluno);
+	}
+	
+		
+
+}
--- src/main/java/com/sys/eletivas/resources/AlunoResource.java
+++ src/main/java/com/sys/eletivas/resources/AlunoResource.java
@@ -1,17 +1,41 @@
 package com.sys.eletivas.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sys.eletivas.dmain.Aluno;
 
 @RestController
 @RequestMapping(value = "/alunos")
 public class AlunoResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest ok";
+	public List<Aluno> listar() {
+		
+		Integer x = 1234567891;
+		Integer y = 1234567891;
+		if(x.equals(y))
+			System.out.println("números iguais");
+		
+		Aluno a1 =  new Aluno(1L, 1678542L, "Clailson", 1, "123456");
+		Aluno a2 =  new Aluno(2L, 2678542L, "Pedro", 1, "123456");
+		Aluno a3 =  new Aluno(3L, 3678542L, "José", 1, "123456");
+		List<Aluno> lista = new ArrayList<>();
+		lista.add(a1);
+		lista.add(a2);
+		lista.add(a3);
+		
+		return lista;
 	}
 
 
+	
+	
+	
+	
+	
 }
