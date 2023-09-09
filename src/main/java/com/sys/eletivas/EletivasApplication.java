82c0ab9c93b6a4f503b22df89b07cfc449ce446b
Clailson <clailsonjc@gmail.com>
2023-09-09 11:43:18 -0300

    Projeto Criado

---
+++ src/main/java/com/sys/eletivas/EletivasApplication.java
@@ -0,0 +1,13 @@
+package com.sys.eletivas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletivasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletivasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sys/eletivas/EletivasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sys.eletivas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletivasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
